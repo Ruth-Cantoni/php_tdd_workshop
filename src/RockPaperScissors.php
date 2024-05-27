f3ee67250fafd66a630d743be1289f5a46634a82
Ruth Cantoni <Ruth-Cantoni@users.noreply.github.com>
2024-05-27 10:23:11 +0200

    begin sketching something

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Kata;
 
-final class ChangeMe
+final class RockPaperScissors
 {
-    public function changeMe(): bool
+    public function play(): bool
     {
         return true;
     }
---
+++ tests/RockPaperScissorsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KataTests;
+
+use Kata\RockPaperScissors;
+use PHPUnit\Framework\TestCase;
+
+final class RockPaperScissorsTest extends TestCase
+{
+    public function test_rock_beats_scissors(): void
+    {
+        $changeMe = new RockPaperScissors();
+
+        self::assertTrue($changeMe->play());
+    }
+}
