6b9e367532e365af5bde1f5762ceea9e8f3fc54e
Ruth Cantoni <Ruth-Cantoni@users.noreply.github.com>
2024-05-27 14:48:02 +0200

    green: test_return_loose_when_the_user_has_not_guessed_the_number_the_third_time

--- src/GuessRandomNumberGame.php
+++ src/GuessRandomNumberGame.php
@@ -6,20 +6,34 @@ namespace Kata;
 
 final class GuessRandomNumberGame
 {
-
+    const MAX_NUM_OF_ATTEMPTS = 3;
     private int $randomNumber;
+    private int $counter = 0;
 
     public function __construct(RandomNumberGeneratorInterface $randomGenerator) {
         $this->randomNumber = $randomGenerator->getANumber();
     }
     public function play($guessNumber): string
     {
-        if ( $guessNumber > $this->randomNumber) {
+        $this->counter++;
+
+        if ( $guessNumber === $this->randomNumber ) {
+            return 'win';
+        }
+
+        if ( $this->isMaxNumberOfAttemptsExceeded() ) {
+            return 'loose';
+        }
+
+        if ( $guessNumber > $this->randomNumber ) {
             return 'higher';
         }
-        if ( $guessNumber < $this->randomNumber) {
-            return 'lower';
-        }
-        return 'win';
+        return 'lower';
+    }
+
+    protected function isMaxNumberOfAttemptsExceeded() {
+
+        return ($this->counter >= self::MAX_NUM_OF_ATTEMPTS);
+
     }
 }
