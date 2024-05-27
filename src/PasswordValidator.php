b02edc86a0863238b8d10ae93dea11fd32f4eb65
Jirka Hermany <jiri.hermany@clockodo.com>
2024-05-27 11:46:39 +0200

    green: test_short_password_is_invalid

--- src/PasswordValidator.php
+++ src/PasswordValidator.php
@@ -6,8 +6,19 @@ namespace Kata;
 
 final class PasswordValidator
 {
+    const MIN_LENGTH = 8;
+
     public function validate(string $password): bool
     {
+        if ($this->passwordIsTooShort($password)) {
+            return false;
+        }
+
         return true;
+    }
+
+    private function passwordIsTooShort(string $password): bool
+    {
+        return mb_strlen($password) < self::MIN_LENGTH;
     }
 }
